70b02e30bd95fe21891ec68e34cc840f8f6f344f
morozov_ev <morozov_ev@technology45.ru>
2020-01-23 09:48:38 +0300

    Добавлен Dockerfile и обвязку

--- infra/dm-create-host.sh
+++ infra/dm-create-host.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-docker-machine create --driver google \
+docker-machine create \
+--driver google \
 --google-project $1 \
 --google-zone europe-west3-b \
---google-machine-type f1-micro \
+--google-machine-type n1-standard-1 \
 --google-machine-image $(gcloud compute images list --filter ubuntu-1604-lts --uri) \
 docker-host

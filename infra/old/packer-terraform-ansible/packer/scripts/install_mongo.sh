772ad8be0a9b2fdf4a2e5b900967440af0862619
morozov_ev <morozov_ev@technology45.ru>
2020-01-27 13:56:11 +0300

    rename old directory

--- infra/old/packer-terraform-ansible/packer/scripts/install_mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - && \
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list && \
-apt-get update && apt-get install mongodb-org -y 2>/dev/null && \
-systemctl start mongod && \
-systemctl enable mongod
--- infra/old/packer-terraform-ansible/terraform/modules/db/variables.tf
+++
@@ -1,7 +0,0 @@
-variable db_disk_image {
-    description = "Disk image"
-    default     = "raddit-db-base"
-}
-variable public_key_path {
-    description = "Path to the public key used for ssh access"
-}
